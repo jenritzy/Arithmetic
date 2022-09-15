af86e557995dbe29f6ddf4d75e90515e556e8b83
jerit6490 <jerit6490@2Q3H0Q2>
2022-09-15 13:41:49 -0400

    Add project files.

---
+++ Arithmetic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 8;
+        int b = 4;
+        Console.WriteLine("Addition:\t" + (a + b));
+        Console.WriteLine("Subtraction:\t" + (a - b));
+        Console.WriteLine("Multiplication:\t" + (a * b)); 
+        Console.WriteLine("Division:\t" + (a / b));
+        Console.WriteLine("Modulus:\t" + (a % b));
+        Console.WriteLine("\nPostfix:\t" + (a++));
+        Console.WriteLine("Postfix Result....:\t" + a);
+        Console.WriteLine("\nPrefix:\t" + (++b));
+        Console.WriteLine("Prefix Result....:\t" + b);
+        Console.ReadKey();
+    }
+}
